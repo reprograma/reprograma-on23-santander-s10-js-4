05d9ecba32188e82a3653b576ea0f24ce0d4032f
ayndiara <ayndiara.miranda.00@gmail.comgit config --global user.email ayndiara.miranda.00@gmail.ccom>
2023-05-31 16:28:50 -0300

    atividade cep e filme

---
+++ exercicios/ayndiara/projeto-casa/ViaCEP/script.js
@@ -0,0 +1,40 @@
+const container = document.getElementById('demo')
+
+function createFilm(film) {
+  return `
+    <div class="films">
+       <img src = "${film.image}"/>
+      <h2 class="title">${film.title}</h2>
+      <p class="diretor">${film.director}</p>
+      <p class="release-date">${film.release_date}</p>
+      <p class="description">${film.description}</p>
+      
+
+    </div>
+  `
+} 
+
+async function getFilms() {
+  try {
+    const response = await fetch('https://ghibliapi.vercel.app/films')
+    const json = await response.json()
+    //const films = json.data
+    return json
+    //const oursFilms = films.slice(1, 10)
+    //return oursFilms
+  }
+  catch(erro) {
+    console.error("Erro capturado: " + erro)
+  }
+}
+
+async function main() {
+  const films = await getFilms()
+  films.map((film) => container.innerHTML += createFilm(film))
+}
+
+
+
+
+
+main()
